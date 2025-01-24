89479de910aef3579b536ceefa66e1607d52523f
hyjtiny <hyjtiny@hotmail.com>
2025-01-24 22:51:47 +0800

    删除关于vendor的东西

--- omni_ud710_21c10.mk
+++ omni_ud710_21c10.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from ud710_21c10 device
 $(call inherit-product, device/sprd/ud710_21c10/device.mk)
